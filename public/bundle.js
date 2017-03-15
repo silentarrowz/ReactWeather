45b5bbcf712186ca1239889a38b7d1a0779c65fc
silentarrowz <silentarrowz@gmail.com>
2017-03-15 22:18:53 +0530

    updated about

--- public/bundle.js
+++ public/bundle.js
@@ -26587,9 +26587,18 @@
 
 	var About = function About(props) {
 		return React.createElement(
-			'h3',
+			'div',
 			null,
-			'About Component'
+			React.createElement(
+				'h3',
+				null,
+				'About '
+			),
+			React.createElement(
+				'p',
+				null,
+				'Welcome to the about page'
+			)
 		);
 	};
 
